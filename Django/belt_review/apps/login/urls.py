45fa1c07d773fff3bbe14fb1eabd9584e162bcc5
CristyMona11 <cristina.monarrez.11@gmail.com>
2017-06-21 18:16:17 -0500

    a bunch of projects deleted

--- Django/belt_review/apps/login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from views import *
-
-urlpatterns = [
-    url(r'^$', index, name='home'),
-    url(r'^register_user$', register, name='register'),
-    url(r'^logout$', logout, name='logout'),
-    url(r'^auth$', authenticate, name='authenticate'),
-]
--- Django/beltexam/apps/wishlist/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class WishlistConfig(AppConfig):
-    name = 'wishlist'
